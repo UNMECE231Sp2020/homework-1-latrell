5352e33f035b4b775ea81bf8065564517b73bc85
Latrell Racca <ljracca18@gmail.com>
2020-02-09 13:06:45 -0700

    For HW1 I created two functions in my .h and .c file called fnt1 and fnt2 and implemented those functions in my main.c file.

--- main.c
+++ main.c
@@ -1,9 +1,37 @@
 #include <stdio.h>
 #include "my_complex.h"
 
+
 int main() {
 	Complex a = {3, 4};
 	Complex b = {5, 12};
 
+	Complex (*ArrFuncPtr[4])(Complex, Complex);
+
+	ArrFuncPtr[0] = complex_add;
+	ArrFuncPtr[1] = complex_sub;
+	ArrFuncPtr[2] = complex_mult;
+	ArrFuncPtr[3] = complex_div;
+
+	double (*ArrFuncPtr2[2])(Complex);
+
+	ArrFuncPtr2[0] = magnitude;
+	ArrFuncPtr2[1] = phase;
+
+
+
+	for (int i=0; i < 4; i++)
+	{
+		fnt1 (ArrFuncPtr[i], a , b);
+
+	}
+	
+	for (int i=0; i < 2; i++)
+	{
+		fnt2 (ArrFuncPtr2[i], a);
+		fnt2 (ArrFuncPtr2[i], b);
+	}
+
 	return 0;
+	
 }
--- my_complex.h
+++ my_complex.h
@@ -9,6 +9,7 @@ typedef struct _complex Complex;
 
 void print_complex(Complex c);
 
+
 Complex complex_add(Complex c1, Complex c2);
 Complex complex_sub(Complex c1, Complex c2);
 Complex complex_mult(Complex c1, Complex c2);
@@ -18,3 +19,8 @@ Complex complex_conj(Complex c);
 
 double magnitude(Complex c);
 double phase(Complex c);
+
+void fnt1(Complex (*fntptr)(Complex, Complex), Complex a, Complex b);
+void fnt2(double (*fntptr2)(Complex) , Complex x);
+
+	
